5f6db26a7ed9bead4194cdcfc50b13aeca530d61
Jongtaek Choi <lastchoice86@naver.com>
2024-12-16 03:05:41 +0900

    feat: add react-particles

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,31 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+
 import Container from "../ui/Container";
 import { H1 } from "../ui/styled";
 import RowLink from "../ui/RowLink";
+import { loadSlim } from "@tsparticles/slim";
 import mailIcon from "src/assets/email.svg";
 import octicon from "src/assets/octicon.svg";
 import phoneIcon from "src/assets/phone.svg";
+import { useEffect } from "react";
 
 export default function Main() {
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    });
+  }, []);
   return (
     <Container>
+      <Particles
+        options={{
+          particles: {
+            number: { value: 100 },
+            size: { value: 3 },
+            move: { enable: true, speed: 2 },
+          },
+        }}
+      />
       <H1>
         최종택<span className="text-gray-500 text-sm">JongtaekChoi</span>
       </H1>
