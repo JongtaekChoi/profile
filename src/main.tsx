e16cbe731ebe8bc489de43ee2ca163d7a4a8310d
Jongtaek Choi <lastchoice86@naver.com>
2026-02-05 08:02:42 +0900

    Fix blog loading: polyfill Buffer for gray-matter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import "./index.css";
 
+// Some dependencies (e.g., gray-matter) expect Node globals.
+// We polyfill the minimum needed for the browser build.
+import { Buffer } from "buffer";
+(globalThis as any).Buffer = Buffer;
+
 import App from "./App.tsx";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
